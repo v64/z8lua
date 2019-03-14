032c7176671ca1b6e90f230c4b89c4159bac7eed
Lee Witek <5556121+0xcafed00d@users.noreply.github.com>
2019-03-14 11:44:02 +0000

    Revert "fix problems with building with clang on anroid ndk"
    This reverts commit c52038326bdd3d8b3b7d061b30095a18ff4e7de6.

--- fix32.h
+++ fix32.h
@@ -12,288 +12,136 @@
 
 #pragma once
 
-#include <cmath>
 #include <cstdint>
+#include <cmath>
 #include <type_traits>
 
-namespace z8 {
+namespace z8
+{
 
-	struct fix32 {
-		inline fix32() = default;
+struct fix32
+{
+    inline fix32() = default;
 
-		/* Convert from/to double */
-		inline fix32(double d) : m_bits((int32_t)std::round(std::ldexp(d, 16))) {
-		}
+    /* Convert from/to double */
+    inline fix32(double d)
+      : m_bits((int32_t)std::round(std::ldexp(d, 16)))
+    {}
 
-		inline operator double() const {
-			return std::ldexp((double)m_bits, -16);
-		}
+    inline operator double() const
+    {
+        return std::ldexp((double)m_bits, -16);
+    }
 
-		/* Conversions up to int16_t are allowed */
-		inline fix32(int8_t x) : m_bits((int32_t)(x << 16)) {
-		}
-		inline fix32(uint8_t x) : m_bits((int32_t)(x << 16)) {
-		}
-		inline fix32(int16_t x) : m_bits((int32_t)(x << 16)) {
-		}
+    /* Conversions up to int16_t are allowed */
+    inline fix32(int8_t x)  : m_bits((int32_t)(x << 16)) {}
+    inline fix32(uint8_t x) : m_bits((int32_t)(x << 16)) {}
+    inline fix32(int16_t x) : m_bits((int32_t)(x << 16)) {}
 
-		/* Anything above int16_t is risky because of precision loss */
-		inline fix32(uint16_t x) : m_bits((int32_t)(x << 16)) {
-		}
-		inline fix32(int32_t x) : m_bits((int32_t)(x << 16)) {
-		}
-		inline fix32(uint32_t x) : m_bits((int32_t)(x << 16)) {
-		}
-		inline fix32(int64_t x) : m_bits((int32_t)(x << 16)) {
-		}
-		inline fix32(uint64_t x) : m_bits((int32_t)(x << 16)) {
-		}
+    /* Anything above int16_t is risky because of precision loss */
+    inline fix32(uint16_t x) : m_bits((int32_t)(x << 16)) {}
+    inline fix32(int32_t x)  : m_bits((int32_t)(x << 16)) {}
+    inline fix32(uint32_t x) : m_bits((int32_t)(x << 16)) {}
+    inline fix32(int64_t x)  : m_bits((int32_t)(x << 16)) {}
+    inline fix32(uint64_t x) : m_bits((int32_t)(x << 16)) {}
 
-		/* Support for long and unsigned long when it is a distinct
-		 * type from the standard int*_t types, e.g. on Windows. */
-		template <
-		    typename T,
-		    typename std::enable_if<
-		        (std::is_same<T, long>::value || std::is_same<T, unsigned long>::value) &&
-		        !std::is_same<T, int32_t>::value && !std::is_same<T, uint32_t>::value &&
-		        !std::is_same<T, int64_t>::value && !std::is_same<T, uint64_t>::value>::type*...>
-		inline fix32(T x) : m_bits((int32_t)(x << 16)) {
-		}
+    /* Support for long and unsigned long when it is a distinct
+     * type from the standard int*_t types, e.g. on Windows. */
+    template<typename T,
+             typename std::enable_if<(std::is_same<T, long>::value ||
+                                      std::is_same<T, unsigned long>::value) &&
+                                     !std::is_same<T, int32_t>::value &&
+                                     !std::is_same<T, uint32_t>::value &&
+                                     !std::is_same<T, int64_t>::value &&
+                                     !std::is_same<T, uint64_t>::value>::type *...>
+    inline fix32(T x) : m_bits((int32_t)(x << 16)) {}
 
-		/* Explicit casts are all allowed */
-		inline operator int8_t() const {
-			return m_bits >> 16;
-		}
-		inline operator uint8_t() const {
-			return m_bits >> 16;
-		}
-		inline operator int16_t() const {
-			return m_bits >> 16;
-		}
-		inline operator uint16_t() const {
-			return m_bits >> 16;
-		}
-		inline operator int32_t() const {
-			return m_bits >> 16;
-		}
-		inline operator uint32_t() const {
-			return m_bits >> 16;
-		}
-		inline operator int64_t() const {
-			return m_bits >> 16;
-		}
-		inline operator uint64_t() const {
-			return m_bits >> 16;
-		}
+    /* Explicit casts are all allowed */
+    inline operator int8_t() const { return m_bits >> 16; }
+    inline operator uint8_t() const { return m_bits >> 16; }
+    inline operator int16_t() const { return m_bits >> 16; }
+    inline operator uint16_t() const { return m_bits >> 16; }
+    inline operator int32_t() const { return m_bits >> 16; }
+    inline operator uint32_t() const { return m_bits >> 16; }
+    inline operator int64_t() const { return m_bits >> 16; }
+    inline operator uint64_t() const { return m_bits >> 16; }
 
-		template <
-		    typename T,
-		    typename std::enable_if<
-		        (std::is_same<T, long>::value || std::is_same<T, unsigned long>::value) &&
-		        !std::is_same<T, int32_t>::value && !std::is_same<T, uint32_t>::value &&
-		        !std::is_same<T, int64_t>::value && !std::is_same<T, uint64_t>::value>::type*...>
-		inline operator T() const {
-			return (T)(m_bits >> 16);
-		}
+    template<typename T,
+             typename std::enable_if<(std::is_same<T, long>::value ||
+                                      std::is_same<T, unsigned long>::value) &&
+                                     !std::is_same<T, int32_t>::value &&
+                                     !std::is_same<T, uint32_t>::value &&
+                                     !std::is_same<T, int64_t>::value &&
+                                     !std::is_same<T, uint64_t>::value>::type *...>
+    inline operator T() const { return (T)(m_bits >> 16); }
 
-		/* Directly initialise bits */
-		static inline fix32 frombits(int32_t x) {
-			fix32 ret;
-			ret.m_bits = x;
-			return ret;
-		}
+    /* Directly initialise bits */
+    static inline fix32 frombits(int32_t x)
+    {
+        fix32 ret; ret.m_bits = x; return ret;
+    }
 
-		inline int32_t bits() const {
-			return m_bits;
-		}
+    inline int32_t bits() const { return m_bits; }
 
-		/* Comparisons */
-		bool operator==(fix32 x) const {
-			return m_bits == x.m_bits;
-		}
-		bool operator!=(fix32 x) const {
-			return m_bits != x.m_bits;
-		}
-		bool operator<(fix32 x) const {
-			return m_bits < x.m_bits;
-		}
-		bool operator>(fix32 x) const {
-			return m_bits > x.m_bits;
-		}
-		bool operator<=(fix32 x) const {
-			return m_bits <= x.m_bits;
-		}
-		bool operator>=(fix32 x) const {
-			return m_bits >= x.m_bits;
-		}
+    /* Comparisons */
+    bool operator ==(fix32 x) const { return m_bits == x.m_bits; }
+    bool operator !=(fix32 x) const { return m_bits != x.m_bits; }
+    bool operator  <(fix32 x) const { return m_bits  < x.m_bits; }
+    bool operator  >(fix32 x) const { return m_bits  > x.m_bits; }
+    bool operator <=(fix32 x) const { return m_bits <= x.m_bits; }
+    bool operator >=(fix32 x) const { return m_bits >= x.m_bits; }
 
-		/* Increments */
-		fix32& operator++() {
-			m_bits += 0x10000;
-			return *this;
-		}
-		fix32& operator--() {
-			m_bits -= 0x10000;
-			return *this;
-		}
-		fix32 operator++(int) {
-			fix32 ret = *this;
-			++*this;
-			return ret;
-		}
-		fix32 operator--(int) {
-			fix32 ret = *this;
-			--*this;
-			return ret;
-		}
+    /* Increments */
+    fix32& operator ++() { m_bits += 0x10000; return *this; }
+    fix32& operator --() { m_bits -= 0x10000; return *this; }
+    fix32 operator ++(int) { fix32 ret = *this; ++*this; return ret; }
+    fix32 operator --(int) { fix32 ret = *this; --*this; return ret; }
 
-		/* Math operations */
-		fix32 const& operator+() const {
-			return *this;
-		}
-		fix32 operator-() const {
-			return frombits(-m_bits);
-		}
-		fix32 operator~() const {
-			return frombits(~m_bits);
-		}
+    /* Math operations */
+    fix32 const &operator +() const { return *this; }
+    fix32 operator -() const { return frombits(-m_bits); }
+    fix32 operator ~() const { return frombits(~m_bits); }
 
-		fix32 operator+(fix32 x) const {
-			return frombits(m_bits + x.m_bits);
-		}
-		fix32 operator-(fix32 x) const {
-			return frombits(m_bits - x.m_bits);
-		}
-		fix32 operator&(fix32 x) const {
-			return frombits(m_bits & x.m_bits);
-		}
-		fix32 operator|(fix32 x) const {
-			return frombits(m_bits | x.m_bits);
-		}
-		fix32 operator^(fix32 x) const {
-			return frombits(m_bits ^ x.m_bits);
-		}
+    fix32 operator +(fix32 x) const { return frombits(m_bits + x.m_bits); }
+    fix32 operator -(fix32 x) const { return frombits(m_bits - x.m_bits); }
+    fix32 operator &(fix32 x) const { return frombits(m_bits & x.m_bits); }
+    fix32 operator |(fix32 x) const { return frombits(m_bits | x.m_bits); }
+    fix32 operator ^(fix32 x) const { return frombits(m_bits ^ x.m_bits); }
 
-		fix32 operator*(fix32 x) const {
-			return frombits((int64_t)m_bits * x.m_bits / 0x10000);
-		}
+    fix32 operator *(fix32 x) const
+    {
+        return frombits((int64_t)m_bits * x.m_bits / 0x10000);
+    }
 
-		fix32 operator/(fix32 x) const {
-			/* XXX: PICO-8 returns 0x8000.0001 instead of 0x8000.0000 */
-			if (x.m_bits == 0)
-				return frombits(m_bits >= 0 ? 0x7fffffff : 0x80000001);
-			return frombits((int64_t)m_bits * 0x10000 / x.m_bits);
-		}
+    fix32 operator /(fix32 x) const
+    {
+        /* XXX: PICO-8 returns 0x8000.0001 instead of 0x8000.0000 */
+        if (x.m_bits == 0)
+            return frombits(m_bits >= 0 ? 0x7fffffff : 0x80000001);
+        return frombits((int64_t)m_bits * 0x10000 / x.m_bits);
+    }
 
-		inline fix32& operator+=(fix32 x) {
-			return *this = *this + x;
-		}
-		inline fix32& operator-=(fix32 x) {
-			return *this = *this - x;
-		}
-		inline fix32& operator&=(fix32 x) {
-			return *this = *this & x;
-		}
-		inline fix32& operator|=(fix32 x) {
-			return *this = *this | x;
-		}
-		inline fix32& operator^=(fix32 x) {
-			return *this = *this ^ x;
-		}
-		inline fix32& operator*=(fix32 x) {
-			return *this = *this * x;
-		}
-		inline fix32& operator/=(fix32 x) {
-			return *this = *this / x;
-		}
+    inline fix32& operator +=(fix32 x) { return *this = *this + x; }
+    inline fix32& operator -=(fix32 x) { return *this = *this - x; }
+    inline fix32& operator &=(fix32 x) { return *this = *this & x; }
+    inline fix32& operator |=(fix32 x) { return *this = *this | x; }
+    inline fix32& operator ^=(fix32 x) { return *this = *this ^ x; }
+    inline fix32& operator *=(fix32 x) { return *this = *this * x; }
+    inline fix32& operator /=(fix32 x) { return *this = *this / x; }
 
-		/* Free functions */
-		static fix32 abs(fix32 a) {
-			return a.m_bits > 0 ? a : -a;
-		}
-		static fix32 min(fix32 a, fix32 b) {
-			return a < b ? a : b;
-		}
-		static fix32 max(fix32 a, fix32 b) {
-			return a > b ? a : b;
-		}
+    /* Free functions */
+    static fix32 abs(fix32 a) { return a.m_bits > 0 ? a : -a; }
+    static fix32 min(fix32 a, fix32 b) { return a < b ? a : b; }
+    static fix32 max(fix32 a, fix32 b) { return a > b ? a : b; }
 
-		static fix32 ceil(fix32 x) {
-			return -floor(-x);
-		}
-		static fix32 floor(fix32 x) {
-			return frombits(x.m_bits & 0xffff0000);
-		}
+    static fix32 ceil(fix32 x) { return -floor(-x); }
+    static fix32 floor(fix32 x) { return frombits(x.m_bits & 0xffff0000); }
 
-		static fix32 pow(fix32 x, fix32 y) {
-			return fix32(std::pow((double)x, (double)y));
-		}
+    static fix32 pow(fix32 x, fix32 y) { return fix32(std::pow((double)x, (double)y)); }
 
-		static fix32 fabs(fix32 x) {
-			return fix32(std::fabs((double)x));
-		}
+private:
+    int32_t m_bits;
+};
 
-		static fix32 sin(fix32 x) {
-			return fix32(std::sin((double)x));
-		}
-		static fix32 sinh(fix32 x) {
-			return fix32(std::sinh((double)x));
-		}
-		static fix32 asin(fix32 x) {
-			return fix32(std::asin((double)x));
-		}
+}
 
-		static fix32 cos(fix32 x) {
-			return fix32(std::cos((double)x));
-		}
-		static fix32 cosh(fix32 x) {
-			return fix32(std::cosh((double)x));
-		}
-		static fix32 acos(fix32 x) {
-			return fix32(std::acos((double)x));
-		}
-		static fix32 tan(fix32 x) {
-			return fix32(std::tan((double)x));
-		}
-		static fix32 tanh(fix32 x) {
-			return fix32(std::tanh((double)x));
-		}
-		static fix32 atan(fix32 x) {
-			return fix32(std::atan((double)x));
-		}
-		static fix32 atan2(fix32 x, fix32 y) {
-			return fix32(std::atan2((double)x, (double)y));
-		}
-		static fix32 fmod(fix32 x, fix32 y) {
-			return fix32(std::fmod((double)x, (double)y));
-		}
-		static fix32 modf(fix32 x, fix32* y) {
-			double dy;
-			fix32 r = fix32(std::modf((double)x, &dy));
-			*y = fix32(dy);
-			return r;
-		}
-		static fix32 sqrt(fix32 x) {
-			return fix32(std::sqrt((double)x));
-		}
-		static fix32 log(fix32 x) {
-			return fix32(std::log((double)x));
-		}
-		static fix32 log10(fix32 x) {
-			return fix32(std::log10((double)x));
-		}
-		static fix32 exp(fix32 x) {
-			return fix32(std::exp((double)x));
-		}
-		static fix32 frexp(fix32 x, int* i) {
-			return fix32(std::frexp((double)x, i));
-		}
-		static fix32 ldexp(fix32 x, int i) {
-			return fix32(std::ldexp((double)x, i));
-		}
-
-	   private:
-		int32_t m_bits;
-	};
-
-}  // namespace z8
